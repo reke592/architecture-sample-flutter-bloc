12058601589d305ca7b3ec91254d1419ddd3f7b9
LAPTOP-R0JLB3EV\Erric <erric.rapsing@gmail.com>
2023-12-08 22:20:23 +0800

    add missing listener to reset the comment form

--- packages/issues/lib/src/presentation/issue_overview/issue_overview_screen.dart
+++ packages/issues/lib/src/presentation/issue_overview/issue_overview_screen.dart
@@ -41,6 +41,7 @@ class IssueOverviewScreen extends StatelessWidget {
           onAcceptSelectionData,
           onAcceptSelectionData,
           onLoadOverview,
+          onSubmitComment,
         ],
         child: Scaffold(
           appBar: AppBar(
